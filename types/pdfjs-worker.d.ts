be1869cfb943d53dacce5ebda3411218a912a8b3
Jafarscript <lihammedjafar@gmail.com>
2025-04-28 11:25:46 +0100

    canvas error on build

--- types/pdfjs-worker.d.ts
+++ types/pdfjs-worker.d.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
 declare module 'pdfjs-dist/legacy/build/pdf.worker.entry';
 // declare module "pdfjs-dist/build/pdf" {
 //     const pdfjsLib: any;
